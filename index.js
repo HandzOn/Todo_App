79839114d1444a806feaadc6b11c0f0b7f5fbccf
Guilherme França <guii_real@hotmail.com>
2020-01-01 23:21:26 -0300

    Complete application

--- index.js
+++ index.js
@@ -9,12 +9,36 @@ var todos = [
 ];
 
 function renderTodos() {
+  listElement.innerHTML = '';
   for (var todo of todos) {
     var todoElement = document.createElement('li');
     var todoText = document.createTextNode(todo);
+    var linkElement = document.createElement('a');
+    linkElement.setAttribute('href', '#');
+    var position = todos.indexOf(todo);
+    linkElement.setAttribute('onclick', 'deleteTodo(' + position + ')');
+    var linkText = document.createTextNode('Excluir');
+
+    linkElement.appendChild(linkText);
     todoElement.appendChild(todoText);
+    todoElement.appendChild(linkElement);
+
     listElement.appendChild(todoElement);
   }
 }
 
+function addTodo() {
+  var todoText = inputElement.value;
+  todos.push(todoText);
+  inputElement.value = '';
+  renderTodos();
+}
+
+function deleteTodo(position) {
+  todos.splice(position, 1);
+  renderTodos();
+}
+
 renderTodos();
+
+buttonElement.onclick = addTodo;
